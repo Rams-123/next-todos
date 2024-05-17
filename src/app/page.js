64de6780c5d1528f5e2ea9456e757a118bf7ba57
Rams-123 <rams@letsupgrade.in>
2024-05-17 22:28:16 +0530

    fit the ui

--- src/app/page.js
+++ src/app/page.js
@@ -8,32 +8,32 @@ import { Checkbox } from "@/components/ui/checkbox"
 export default function Home() {
   const tasks = Array.from({ length: 8 }, (_, i) => i + 1);
   return (
-    <section className="relative flex flex-col h-screen w-full max-w-md mx-auto border border-slate-500 p-5 rounded-2xl gap-4">
-      <div className=" flex flex-col w-full gap-4 ">
-        <div className="">
-          <h1 className="text-3xl font-bold">Today Todos</h1>
-        </div>
+    <section className="relative flex flex-col h-screen w-full max-w-md mx-auto p-4">
+      <div className="  flex flex-col w-full border border-slate-500 p-5 rounded-2xl gap-4">
+        <div className=" flex flex-col w-full gap-4  ">
+          <div className="">
+            <h1 className="text-3xl font-bold">Today Todos</h1>
+          </div>
 
-        <div className="flex flex-col flex-1 gap-4 ">
-          {tasks.map((task, index) => (
-            <div key={index} className="flex flex-1 border shadow-lg border-slate-500 px-5 py-3 items-center justify-between rounded-md">
-              <div className="flex w-full items-center text-center">
-                <input type="checkbox" className="items-center" />
-                <p className="px-5 w-full flex flex-1">This is an example of task {task}</p>
-                <button className="hover:text-red-500 rounded-md" aria-label="Delete Task">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                  </svg>
+          <div className="flex flex-col flex-1 gap-4 ">
+            {tasks.map((task, index) => (
+              <div key={index} className="flex flex-1 border shadow-lg border-slate-500 px-5 py-3 items-center justify-between rounded-md">
+                <div className="flex w-full items-center text-center">
+                  <input type="checkbox" className="items-center" />
+                  <p className="px-5 w-full flex flex-1">This is an example of task {task}</p>
+                  <button className="hover:text-red-500 rounded-md" aria-label="Delete Task">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
 
-                </button>
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
 
-      </div>
-      <div className="absolute flex w-full bottom-0 left-0 items-center p-2 justify-center">
-        <Button className="flex   bg-blue-800 flex-1">Add task</Button>
+        </div>
+        <Button className="flex bg-blue-800 flex-1">Add task</Button>
       </div>
 
       {/* <div className="relative p-4 mx-auto w-[370px]">
